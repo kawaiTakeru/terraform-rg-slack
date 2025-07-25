6a5f83fdbec299177a9b30b95d8bc5bc45fdfedf
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-25 13:21:03 +0900

    Update vnet.tf

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -1,7 +1,7 @@
 # vnet.tf
 
 resource "azurerm_virtual_network" "example_vnet" {
-  name                = "vnet-from-pipeline"
+  name                = "vnet-from-pipeline-99"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
