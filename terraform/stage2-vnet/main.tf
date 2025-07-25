44bd0dec0c10f849d508515e32bf6fd0efa0fe7b
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-25 13:38:03 +0900

    Update main.tf

--- terraform/stage2-vnet/main.tf
+++ terraform/stage2-vnet/main.tf
@@ -1,1 +1,11 @@
+data "azurerm_resource_group" "example" {
+  name = "rg-from-pipeline"
+}
+
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "vnet-from-pipeline"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+}
 
