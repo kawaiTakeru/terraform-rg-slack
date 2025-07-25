57b5eb8f5566bffaabc0cab35f3f6ee5661397a9
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-25 13:16:18 +0900

    Create vnet.tf

---
+++ terraform/vnet.tf
@@ -0,0 +1,8 @@
+# vnet.tf
+
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "vnet-from-pipeline"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
