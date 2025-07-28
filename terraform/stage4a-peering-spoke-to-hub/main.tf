37b7675e045bd4f50a8ce4e2347665fda5787924
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-28 15:18:39 +0900

    Update main.tf

--- terraform/stage4a-peering-spoke-to-hub/main.tf
+++ terraform/stage4a-peering-spoke-to-hub/main.tf
@@ -1,20 +1,28 @@
 provider "azurerm" {
   features {}
+  subscription_id = "6a018b75-55b5-4b68-960d-7328148568aa" # ← Spoke側サブスクリプションID
+}
+
+provider "azurerm" {
+  alias           = "hub"
+  subscription_id = "7d1f78e5-bc6c-4018-847f-336ff47b9436" # ← Hub側サブスクリプションID
+  features        = {}
 }
 
 data "azurerm_virtual_network" "spoke" {
+  provider            = azurerm
   name                = "vnet-from-pipeline"
   resource_group_name = "rg-from-pipeline"
-  subscription_id     = "6a018b75-55b5-4b68-960d-7328148568aa" # ← Spoke側
 }
 
 data "azurerm_virtual_network" "hub" {
+  provider            = azurerm.hub
   name                = "vnet-test-hubnw-prd-jpe-001"
   resource_group_name = "rg-test-hubnw-prd-jpe-001"
-  subscription_id     = "7d1f78e5-bc6c-4018-847f-336ff47b9436" # ← Hub側
 }
 
 resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+  provider                  = azurerm
   name                      = "spoke-to-hub"
   resource_group_name       = data.azurerm_virtual_network.spoke.resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.spoke.name
